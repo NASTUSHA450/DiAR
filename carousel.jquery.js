ce424b0e47b39bed1c5daea4c66f8b274158f651
NASTUSHA450 <nastyadragunskaya@gmail.com>
2024-07-09 14:14:04 +0500

    fix

--- carousel.jquery.js
+++ carousel.jquery.js
@@ -1,5 +1,12 @@
 (function ($) {
     var methods = {
+        /**
+         * Initialise the plugin.
+         *
+         * @param object options Object of options for this plugin
+         *
+         * @throws Exception     If the animation setting has an invalid value
+         */
         init: function (options) {
             var defaults = {
                 arrows: false,   // Show arrow controls
@@ -17,6 +24,17 @@
             },
                 settings = $.extend({}, defaults, options);
 
+            /**
+             * Checks if a variable is a valid object with named properties
+             * passed in the 'arrows' option.
+             *
+             * The object must have a property named 'next' and one either named
+             * 'prev' or 'previous'. The value of both of these must be a jQuery
+             * object.
+             *
+             * @param  mixed input The variable to check
+             * @return boolean     True if the input is valid, false otherwise
+             */
             function isArrowControlObject(input) {
                 if (typeof input !== 'object') {
                     return false;
@@ -50,13 +68,17 @@
                         current: 0
                     };
 
+                // Validate the settings
                 if (settings.animation !== 'slide') {
                     $.error('Unsupported animation type for jQuery.carousel: ' + settings.animation);
                 }
 
+                // Wrap slides in an element we can use as a "window" to the slides
                 self.wrap($('<div>'));
 
                 var wrap = self.parent();
+
+                // Set the basic structural styling for the carousel
                 wrap.css({
                     width: '100%',
                     overflow: 'hidden'
@@ -68,20 +90,25 @@
 
                 self.children(':first').addClass('carousel-active');
 
+                // Initialise arrow controls
                 if (settings.arrows !== false) {
+                    // An object with named properties for next & previous/prev
                     if (isArrowControlObject(settings.arrows)) {
                         state.arrows.next = settings.arrows.next;
                         state.arrows.previous = (settings.arrows.hasOwnProperty('previous')) ? settings.arrows.previous : settings.arrows.prev;
                     }
+                    // An array with two elements, the first is assumed the prev control
                     else if (typeof settings.arrows === 'object' && settings.arrows.length === 2) {
                         state.arrows.next = settings.arrows[1];
                         state.arrows.previous = settings.arrows[0];
                     }
+                    // Generate the arrows
                     else if (settings.arrows === true) {
-                        state.arrows.next = $('<button class="arrow next backgound-set"></button>').insertAfter(wrap);
-                        state.arrows.previous = $('<button class="arrow previous backgound-set"></button>').insertAfter(wrap);
+                        state.arrows.next = $('<button class="arrow next"></button>').insertAfter(wrap);
+                        state.arrows.previous = $('<button class="arrow previous"></button>').insertAfter(wrap);
                     }
 
+                    // Set events for the arrow controls
                     state.arrows.next.on('click.carousel', function () {
                         methods.goToNext.call(self);
                     });
@@ -90,14 +117,82 @@
                     });
                 }
 
+                // Initialise indicators
+                if (settings.indicators !== false) {
+                    var indicatorHTML = $('<ol class="indicators"></ol>');
+
+                    // Create each indicator
+                    self.children().each(function () {
+                        indicatorHTML.append($(`<li>${$(this).index()+1}</li>`).click(function () {
+                            methods.goTo.call(self, $(this).index());
+                        }));
+                    });
+
+                    // jQuery selector for the destination of the indicators
+                    if (typeof settings.indicators === 'object' && settings.indicators instanceof jQuery) {
+                        state.indicators = indicatorHTML.insertAfter(settings.indicators.html);
+                    }
+                    // Generate the indicators
+                    else if (settings.indicators === true) {
+                        state.indicators = indicatorHTML.insertAfter(wrap);
+                    }
+                }
+
+                // Bind the interaction event, if defined
+                if (typeof settings.onInteraction === 'function') {
+                    var controls = $();
+
+                    if (state.indicators !== false) {
+                        controls = controls.add(state.indicators.children());
+                    }
+
+                    if (state.arrows.previous !== false) {
+                        controls = controls.add(state.arrows.previous);
+                    }
+
+                    if (state.arrows.next !== false) {
+                        controls = controls.add(state.arrows.next);
+                    }
+
+                    controls.on('click.carousel', function () {
+                        settings.onInteraction(self);
+                    });
+                }
+
+                // Save state on the element for use later
                 self.data('carousel', state);
 
+                // Calculate the dimensions
                 methods.setWidth.call(self, wrap.width());
 
+                // If this is a flexible carousel, set resize listeners to set the width
+                if (settings.flexible === true) {
+                    $(window).on('resize.carousel orientationchange.carousel', function () {
+                        methods.detectWidthChange.call(self);
+                    });
+                }
+
+                // Set the initial state (first slide)
                 methods.updateState.call(self);
 
+                // Fire the onReady event, if defined
+                if (typeof settings.onReady === 'function') {
+                    settings.onReady(self);
+                }
+
+                // Start the interval, if defined
+                methods.start.call(self);
             });
         },
+
+        /**
+         * Detect if the wrapper's width has changed.
+         *
+         * The wrapper's width is always 100%, so the computed width can change
+         * if a parent element's width has changed.
+         *
+         * @return object jQuery object for the selected elements, for chaining
+         */
         detectWidthChange: function () {
             return this.each(function () {
                 var self = $(this),
@@ -108,6 +203,19 @@
                 }
             });
         },
+
+        /**
+         * Set the width of the carousel.
+         *
+         * This sets the width for each slide, and sets the total width of the
+         * slide container (the list element) to the new width * number of slides.
+         *
+         * @param  int slideWidth New width for the carousel
+         *
+         * @return object         jQuery object for the selected elements, for chaining
+         *
+         * @throws Exception      If the width passed is not a number or is < 0
+         */
         setWidth: function (slideWidth) {
             width = Number(slideWidth);
 
@@ -119,15 +227,28 @@
                 var self = $(this),
                     state = self.data('carousel');
 
+                // Check the width is actually different
                 if (width !== state.width) {
+                    // Update the width on the state
                     state.width = width;
 
+                    // Update the slides & slide list width
                     self.css('width', width * state.slideCount)
                         .children().css('width', width);
                 }
             });
         },
 
+        /**
+         * Go to a specific slide.
+         *
+         * @param  int slideIndex Index of the slide to go to
+         *
+         * @return object jQuery object for the selected elements, for chaining
+         *
+         * @throws Exception If the slide index does not exist, is not a number
+         *                   or is >= the number of slides
+         */
         goTo: function (slideIndex) {
             index = Number(slideIndex);
 
@@ -136,23 +257,32 @@
                     state = self.data('carousel'),
                     offset = (100 / state.slideCount) * index;
 
+                // Validate the index
                 if (isNaN(index) || index < 0 || index >= state.slideCount) {
                     $.error('Invalid slide reference on jQuery.carousel.goTo: ' + slideIndex);
                 }
+
+                // Set the current state
                 state.current = index;
+
+                // Change the active class
                 self.children().removeClass('carousel-active').eq(index).addClass('carousel-active');
+
+                // Fire the onChangeStart event, if defined
                 if (typeof state.settings.onChangeStart === 'function') {
                     state.settings.onChangeStart(self);
                 }
 
+                // Animate the change of slide
                 self.animate({ marginLeft: '-' + (100 * index) + '%' }, {
                     duration: state.settings.speed,
                     complete: function () {
-
+                        // Fire the onChangeEnd event, if defined
                         if (typeof state.settings.onChangeEnd === 'function') {
                             state.settings.onChangeEnd(self);
                         }
 
+                        // Fire the onComplete event, if defined & this is the last slide
                         if (index + 1 === state.slideCount && typeof state.settings.onComplete === 'function') {
                             state.settings.onComplete(self);
                         }
@@ -163,18 +293,27 @@
             });
         },
 
+        /**
+         * Update the carousel state.
+         *
+         * This method performs some maintenance tasks such as moving the
+         * `active` class on the indicators and setting `disabled` classes on
+         * the arrows where appropriate.
+         *
+         * @return object jQuery object for the selected elements, for chaining
+         */
         updateState: function () {
             return this.each(function () {
                 var state = $(this).data('carousel');
 
-
+                // Set active class on indicator
                 if (state.indicators !== false) {
                     state.indicators
                         .children().removeClass('active')
                         .filter(':eq(' + state.current + ')').addClass('active');
                 }
 
-
+                // Set disabled class on controls where necessary
                 if (state.arrows.previous !== false) {
                     state.arrows.previous.toggleClass('disabled', (state.current === 0));
                 }
@@ -185,11 +324,20 @@
             });
         },
 
-
+        /**
+         * Go to the first slide in the carousel.
+         *
+         * @return object jQuery object for the selected elements, for chaining
+         */
         goToStart: function () {
             return methods.goTo.call(this, 0);
         },
 
+        /**
+         * Go to the last slide.
+         *
+         * @return object jQuery object for the selected elements, for chaining
+         */
         goToEnd: function () {
             return this.each(function () {
                 var self = $(this);
@@ -197,6 +345,15 @@
                 return methods.goTo.call(self, self.data('carousel').slideCount - 1);
             });
         },
+
+        /**
+         * Go to the slide after the currently active one.
+         *
+         * If the currently active slide is the last slide, nothing happens and
+         * no error is raised.
+         *
+         * @return object jQuery object for the selected elements, for chaining
+         */
         goToNext: function () {
             return this.each(function () {
                 var self = $(this);
@@ -210,6 +367,14 @@
             });
         },
 
+        /**
+         * Go to the slide before the currently active one.
+         *
+         * If the currently active slide is the first one, nothing happens and
+         * no error is raised.
+         *
+         * @return object jQuery object for the selected elements, for chaining
+         */
         goToPrevious: function () {
             return this.each(function () {
                 var self = $(this);
@@ -223,20 +388,39 @@
             });
         },
 
+        /**
+         * Cycle between slides.
+         *
+         * This method works similary to `goToNext`. The difference is that this
+         * method returns the carousel to the first slide if the currently
+         * active slide is the last one.
+         *
+         * @return object jQuery object for the selected elements, for chaining
+         */
         cycle: function () {
             return this.each(function () {
                 var self = $(this),
                     state = self.data('carousel');
+
+                // If we're on the last slide, rewind back to the start
                 if (state.current + 1 === state.slideCount) {
                     methods.goToStart.call(self);
                 }
+                // Otherwise, go to next
                 else {
                     methods.goToNext.call(self);
                 }
             });
         },
 
-
+        /**
+         * Start the carousel automatic cycling, if it has been defined in the
+         * settings.
+         *
+         * @return object jQuery object for the selected elements, for chaining
+         *
+         * @throws Exception If the speed setting is not set, or <= 0
+         */
         start: function () {
             return this.each(function () {
                 var self = $(this),
@@ -256,6 +440,11 @@
             });
         },
 
+        /**
+         * Stop the carousel's automatic cycling, if it is currently running.
+         *
+         * @return object jQuery object for the selected elements, for chaining
+         */
         stop: function () {
             return this.each(function () {
                 var state = $(this).data('carousel');
@@ -268,6 +457,7 @@
     };
 
     $.fn.carousel = function (method) {
+        // Method calling logic
         if (methods[method]) {
             return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
         } else if (typeof method === 'object' || !method) {
--- main.js
+++ main.js
@@ -1,98 +1,150 @@
 $(function () {
-    $('.carousel.auto-stop-at-end ol').carousel({
-        arrows: true,
-        indicators: false,
-        interval: 0,
-        speed: 300,
-        onComplete: function (e) {
-            e.carousel('stop');
-        }
-    });
+  $('.carousel.auto-stop-at-end ol').carousel({
+    arrows: true,
+    indicators: true,
+    interval: 0,
+    speed: 300,
+    flexible: true,
+    onComplete: function (e) {
+      e.carousel('stop');
+    }
+  });
 });
 $(function () {
-    $("#tabs").tabs();
-});
-$(function () {
-    $("#tabs1").tabs();
+  $('.carousel.standard ol').carousel({
+    arrows: true,
+    speed: 800,
+    flexible: true,
+  });
 });
 
+// mobile menu
+$("#burger").on("click", function () {
+  $("#mobileMenu").toggleClass("open-menu");
+  if ($('#mobileMenu').hasClass('open-menu')) {
+    $("#burger").html(`<svg width="23" height="24" viewBox="0 0 23 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1 2L22 23" stroke="white" stroke-width="2"/>
+<path d="M22 1L1 22" stroke="white" stroke-width="2"/>
+</svg>`);
+    $('body').addClass("fixed");
+
+  } else {
+    $("#burger").html(`<svg width="38" height="22" viewBox="0 0 38 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <line x1="1" y1="1" x2="37" y2="1" stroke="white" stroke-width="2" stroke-linecap="round" />
+                <line x1="11" y1="11" x2="37" y2="11" stroke="white" stroke-width="2" stroke-linecap="round" />
+                <line x1="1" y1="21" x2="37" y2="21" stroke="white" stroke-width="2" stroke-linecap="round" />
+            </svg>`);
+    $('body').removeClass("fixed");
+  }
+});
+
+// submenu dropdown
 $(document).ready(function () {
-    $(".hoverli").hover(
-  function () {
-     $('ul.file_menu').slideDown('medium');
-  }, 
-  function () {
-     $('ul.file_menu').slideUp('medium');
-  }
-);
+  $(".hoverli").hover(
+    function () {
+      $('ul.file_menu').slideDown('medium');
+    },
+    function () {
+      $('ul.file_menu').slideUp('medium');
+    }
+  );
 
-     $(".file_menu li").hover(
-  function () {
-     $(this).children("ul").slideDown('medium');
-  },
-  function () {
-    $(this).children("ul").slideUp('medium');
-  }
-);
+  $(".file_menu li").hover(
+    function () {
+      $(this).children("ul").slideDown('medium');
+    },
+    function () {
+      $(this).children("ul").slideUp('medium');
+    }
+  );
+  $(".hoverli1").hover(
+    function () {
+      $('ul.file_menu1').slideDown('medium');
+    },
+    function () {
+      $('ul.file_menu1').slideUp('medium');
+    }
+  );
+
+  $(".file_menu1 li").hover(
+    function () {
+      $(this).children("ul").slideDown('medium');
+    },
+    function () {
+      $(this).children("ul").slideUp('medium');
+    }
+  );
 });
-$(document).ready(function () {
-    $(".hoverli1").hover(
-  function () {
-     $('ul.file_menu1').slideDown('medium');
-  }, 
-  function () {
-     $('ul.file_menu1').slideUp('medium');
-  }
-);
+// youtube modal
+function Show() {
+  $("#dialog:ui-dialog").dialog("destroy");
 
-     $(".file_menu1 li").hover(
-  function () {
-     $(this).children("ul").slideDown('medium');
-  },
-  function () {
-    $(this).children("ul").slideUp('medium');
-  }
-);
-});
-
-
-function goToByScroll(id){
-  id = id.replace("Link", "");
-  $('html,body').animate({
-      scrollTop: $("#"+id).offset().top},
-      'slow');
+  $("#dialog-modal").dialog({
+    height: 470,
+    width: 550,
+    modal: true
+  });
 }
 
-$("#info > a").click(function(e) { 
-  e.preventDefault(); 
-  goToByScroll($(this).attr("id"));           
+// types tabs
+$(function () {
+  $("#tabs").tabs();
+  $("#tabs1").tabs();
 });
 
-$('img[data-enlargeable]').addClass('img-enlargeable').click(function() {
-    var src = $(this).attr('src');
-    var modal;
-  
-    function removeModal() {
-      modal.remove();
-      $('body').off('keyup.modal-close');
-    }
-    modal = $('<div>').css({
-      background: 'RGBA(0,0,0,.5) url(' + src + ') no-repeat center',
-      backgroundSize: 'contain',
-      width: '100%',
-      height: '100%',
-      position: 'fixed',
-      zIndex: '10000',
-      top: '0',
-      left: '0',
-      cursor: 'zoom-out'
-    }).click(function() {
+
+
+// scroll to #
+function goToByScroll(id) {
+  id = id.replace("Link", "");
+  $('html,body').animate({
+    scrollTop: $("#" + id).offset().top
+  },
+    'slow');
+}
+
+$("#info > a").click(function (e) {
+  e.preventDefault();
+  goToByScroll($(this).attr("id"));
+});
+$("#infoLink").click(function (e) {
+  console.log('clicked')
+  e.preventDefault();
+  goToByScroll($(this).attr("id"));
+});
+
+// language switch
+$(".language > a").click(function (e) {
+  e.preventDefault();
+  $(".language > a").toggleClass("active")
+})
+
+// enlarge image on click
+$('img[data-enlargeable]').addClass('img-enlargeable').click(function () {
+  var src = $(this).attr('src');
+  var modal;
+
+  function removeModal() {
+    modal.remove();
+    $('body').off('keyup.modal-close');
+  }
+  modal = $('<div>').css({
+    background: 'RGBA(0,0,0,.5) url(' + src + ') no-repeat center',
+    backgroundSize: 'contain',
+    width: '100%',
+    height: '100%',
+    position: 'fixed',
+    zIndex: '10000',
+    top: '0',
+    left: '0',
+    cursor: 'zoom-out'
+  }).click(function () {
+    removeModal();
+  }).appendTo('body');
+  //handling ESC
+  $('body').on('keyup.modal-close', function (e) {
+    if (e.key === 'Escape') {
       removeModal();
-    }).appendTo('body');
-    //handling ESC
-    $('body').on('keyup.modal-close', function(e) {
-      if (e.key === 'Escape') {
-        removeModal();
-      }
-    });
+    }
   });
+});
