45a22eceb4856e5cfc52ddb6cf121893a0962911
NASTUSHA450 <nastyadragunskaya@gmail.com>
2024-07-11 10:07:27 +0500

    menu fixed

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 $(function () {
-  $('.carousel.auto-stop-at-end ol').carousel({
+  $(".carousel.auto-stop-at-end ol").carousel({
     arrows: true,
     indicators: true,
     interval: 0,
     speed: 300,
     flexible: true,
     onComplete: function (e) {
-      e.carousel('stop');
-    }
+      e.carousel("stop");
+    },
   });
 });
 $(function () {
-  $('.carousel.standard ol').carousel({
+  $(".carousel.standard ol").carousel({
     arrows: true,
     speed: 800,
     flexible: true,
@@ -21,57 +21,66 @@ $(function () {
 // mobile menu
 $("#burger").on("click", function () {
   $("#mobileMenu").toggleClass("open-menu");
-  if ($('#mobileMenu').hasClass('open-menu')) {
-    $("#burger").html(`<svg width="23" height="24" viewBox="0 0 23 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  SetMenu();
+});
+$(".mobile-menu-link").on("click", function () {
+  $("#mobileMenu").toggleClass("open-menu");
+  SetMenu();
+});
+
+function SetMenu() {
+  if ($("#mobileMenu").hasClass("open-menu")) {
+    $("#burger")
+      .html(`<svg width="23" height="24" viewBox="0 0 23 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M1 2L22 23" stroke="white" stroke-width="2"/>
 <path d="M22 1L1 22" stroke="white" stroke-width="2"/>
 </svg>`);
-    $('body').addClass("fixed");
-
+    $("body").addClass("fixed");
   } else {
-    $("#burger").html(`<svg width="38" height="22" viewBox="0 0 38 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+    $("#burger")
+      .html(`<svg width="38" height="22" viewBox="0 0 38 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <line x1="1" y1="1" x2="37" y2="1" stroke="white" stroke-width="2" stroke-linecap="round" />
                 <line x1="11" y1="11" x2="37" y2="11" stroke="white" stroke-width="2" stroke-linecap="round" />
                 <line x1="1" y1="21" x2="37" y2="21" stroke="white" stroke-width="2" stroke-linecap="round" />
             </svg>`);
-    $('body').removeClass("fixed");
+    $("body").removeClass("fixed");
   }
-});
+}
 
 // submenu dropdown
 $(document).ready(function () {
   $(".hoverli").hover(
     function () {
-      $('ul.file_menu').slideDown('medium');
+      $("ul.file_menu").slideDown("medium");
     },
     function () {
-      $('ul.file_menu').slideUp('medium');
+      $("ul.file_menu").slideUp("medium");
     }
   );
 
   $(".file_menu li").hover(
     function () {
-      $(this).children("ul").slideDown('medium');
+      $(this).children("ul").slideDown("medium");
     },
     function () {
-      $(this).children("ul").slideUp('medium');
+      $(this).children("ul").slideUp("medium");
     }
   );
   $(".hoverli1").hover(
     function () {
-      $('ul.file_menu1').slideDown('medium');
+      $("ul.file_menu1").slideDown("medium");
     },
     function () {
-      $('ul.file_menu1').slideUp('medium');
+      $("ul.file_menu1").slideUp("medium");
     }
   );
 
   $(".file_menu1 li").hover(
     function () {
-      $(this).children("ul").slideDown('medium');
+      $(this).children("ul").slideDown("medium");
     },
     function () {
-      $(this).children("ul").slideUp('medium');
+      $(this).children("ul").slideUp("medium");
     }
   );
 });
@@ -82,7 +91,7 @@ function Show() {
   $("#dialog-modal").dialog({
     height: 470,
     width: 550,
-    modal: true
+    modal: true,
   });
 }
 
@@ -92,15 +101,15 @@ $(function () {
   $("#tabs1").tabs();
 });
 
-
-
 // scroll to #
 function goToByScroll(id) {
   id = id.replace("Link", "");
-  $('html,body').animate({
-    scrollTop: $("#" + id).offset().top
-  },
-    'slow');
+  $("html,body").animate(
+    {
+      scrollTop: $("#" + id).offset().top,
+    },
+    "slow"
+  );
 }
 
 $("#info > a").click(function (e) {
@@ -108,7 +117,7 @@ $("#info > a").click(function (e) {
   goToByScroll($(this).attr("id"));
 });
 $("#infoLink").click(function (e) {
-  console.log('clicked')
+  console.log("clicked");
   e.preventDefault();
   goToByScroll($(this).attr("id"));
 });
@@ -116,35 +125,40 @@ $("#infoLink").click(function (e) {
 // language switch
 $(".language > a").click(function (e) {
   e.preventDefault();
-  $(".language > a").toggleClass("active")
-})
+  $(".language > a").toggleClass("active");
+});
 
 // enlarge image on click
-$('img[data-enlargeable]').addClass('img-enlargeable').click(function () {
-  var src = $(this).attr('src');
-  var modal;
+$("img[data-enlargeable]")
+  .addClass("img-enlargeable")
+  .click(function () {
+    var src = $(this).attr("src");
+    var modal;
 
-  function removeModal() {
-    modal.remove();
-    $('body').off('keyup.modal-close');
-  }
-  modal = $('<div>').css({
-    background: 'RGBA(0,0,0,.5) url(' + src + ') no-repeat center',
-    backgroundSize: 'contain',
-    width: '100%',
-    height: '100%',
-    position: 'fixed',
-    zIndex: '10000',
-    top: '0',
-    left: '0',
-    cursor: 'zoom-out'
-  }).click(function () {
-    removeModal();
-  }).appendTo('body');
-  //handling ESC
-  $('body').on('keyup.modal-close', function (e) {
-    if (e.key === 'Escape') {
-      removeModal();
+    function removeModal() {
+      modal.remove();
+      $("body").off("keyup.modal-close");
     }
+    modal = $("<div>")
+      .css({
+        background: "RGBA(0,0,0,.5) url(" + src + ") no-repeat center",
+        backgroundSize: "contain",
+        width: "100%",
+        height: "100%",
+        position: "fixed",
+        zIndex: "10000",
+        top: "0",
+        left: "0",
+        cursor: "zoom-out",
+      })
+      .click(function () {
+        removeModal();
+      })
+      .appendTo("body");
+    //handling ESC
+    $("body").on("keyup.modal-close", function (e) {
+      if (e.key === "Escape") {
+        removeModal();
+      }
+    });
   });
-});
